e542959aaae440c19d31d30f95635e73232a3b01
baby <helenwu42@gmail.com>
2018-10-25 09:02:33 -0700

    latest

--- SQL_HW.sql
+++ SQL_HW.sql
@@ -143,9 +143,25 @@ JOIN category c
 ON fc.category_id=c.category_id;
 
 -- 7e. Display the most frequently rented movies in descending order.
+SELECT f.title, COUNT(f.film_id) AS 'Count_of_Rented_Movies'
+FROM  film f
+JOIN inventory i ON (f.film_id= i.film_id)
+JOIN rental r ON (i.inventory_id=r.inventory_id)
+GROUP BY title ORDER BY Count_of_Rented_Movies DESC;
 
 
+-- 7f. Write a query to display how much business, in dollars, each store brought in.
+SELECT s.store_id, sum(p.amount) AS "Total_Sales"
+FROM store s JOIN payment p
+ON s.manager_staff_id = p.staff_id
+GROUP BY p.staff_id;
 
+
+-- 7g. Write a query to display for each store its store ID, city, and country.
+SELECT store_id, city, country FROM store s
+JOIN address a ON (s.address_id=a.address_id)
+JOIN city c ON (a.city_id=c.city_id)
+JOIN country cntry ON (c.country_id=cntry.country_id);
 
 
 
